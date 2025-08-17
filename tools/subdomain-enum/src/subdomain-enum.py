09306173ce0eb3ba776868b2138767a41d550a7e
wrmgt <35582884+warmgt@users.noreply.github.com>
2025-08-17 11:42:38 -0500

    initial draft of script

---
+++ tools/subdomain-enum/src/subdomain-enum.py
@@ -0,0 +1,39 @@
+import requests
+import threading
+
+domain = 'targetdomain.com'
+
+with open('subdomains.txt') as file:
+    subdomains = file.read().splitlines()
+
+discovered_subdomains = []
+
+lock = threading.Lock()
+
+
+def check_subdomains(subdomain):
+
+    url = f'http://{subdomain}.{domain}'
+    try:
+        requests.get(url)
+    except requests.ConnectionError:
+        pass
+    else:
+        print("[+] Discovered subdomain: ", url)
+        with lock:
+            discovered_subdomains.append(url)
+
+
+threads = []
+
+for subdomain in subdomains:
+    thread = threading.Thread(target=check_subdomain, args=(subdomain,))
+    thread.start()
+    threads.append(thread)
+
+for thread in threads:
+    thread.join()
+
+with open("discovered_subdomains.txt", 'w') as f:
+    for subdomain in discovered_subdomains:
+        print(subdomain, file=f)
